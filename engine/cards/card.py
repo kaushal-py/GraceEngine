9c3efb7c0d0af6cb454804b73997031122de5a16
kb-studios <2015kaushal.bhogale@ves.ac.in>
2019-02-27 21:39:00 +0530

    Update abstract card class definition

--- engine/cards/card.py
+++ engine/cards/card.py
@@ -1,3 +1,5 @@
+import re
+
 class Card:
 
     def __init__(self, id, ctype):
@@ -26,10 +28,49 @@ class Card:
         self.id = id
         self.ctype = ctype
 
+        self.stickers = {}
+
+        self.card_links = {}
+
 
     def __validate_and_set_type(self):
 
         ctype_list = self.ctype.split('-')
         valid_list=["TT","BT","RP","LP","LIP","NT"]
+
+        #TODO: Write RE to verify correctness of type.
+
+
+
+    '''
+    Getter and setter methods for the stickers of the card
+    '''
+    def set_sticker_val(self, sticker_key, sticker_val):
+        self.stickers[sticker_key] = sticker_val
+
+    def get_sticker_val(self, sticker_key):
+        return self.stickers[sticker_key]
+    
+
+    '''
+    Getter and setter methods to update the card links. 
+    This is used to combine cards to create a complete program.
+    '''
+    def set_card_link(self, ctype, card_link):
+
+        assert isinstance(card_link, Card)
+        self.card_links[ctype] = card_link
+    
+    def get_card_link(self, ctype):
+        return self.card_links[ctype]
+    
+    
+
+if __name__ == "__main__":
+    c = Card("Variable_setter", "Hello")
+
+
+
+
         
     
