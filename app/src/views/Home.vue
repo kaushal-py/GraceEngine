b72e5532c0fc071f3063b4aab4fa092dfa125143
kb-studios <2015kaushal.bhogale@ves.ac.in>
2019-03-10 15:44:33 +0530

    Install and test bulma

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="home">
+    <section class="hero is-primary">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Hero title
+      </h1>
+      <h2 class="subtitle">
+        Hero subtitle
+      </h2>
+    </div>
+  </div>
+</section>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
