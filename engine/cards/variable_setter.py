78433259e66be1e12c0e49ed1e2aff89a1d31c76
kb-studios <2015kaushal.bhogale@ves.ac.in>
2019-03-13 08:02:52 +0530

    Add generate code to variable_setter

--- engine/cards/variable_setter.py
+++ engine/cards/variable_setter.py
@@ -3,16 +3,42 @@ from sticker import Sticker
 
 class VariableSetter(Card):
 
-    def __init__(self, variable_name:str, variable_value):
+    def __init__(self, variable_name:str):
         card_id = "variable_setter"
         card_type=[True,True,True,False,0,False] #TODO: verify this
         super(VariableSetter, self).__init__(card_id, card_type)
 
         stick_variable_name = Sticker("Variable",variable_name) # TODO: Not sure how to pass sticker type
-        stick_variable_value = Sticker("Value",variable_value) # TODO: Object Expression
+        # stick_variable_value = Sticker("Value",variable_value) # TODO: Object Expression
         self.variable_name = stick_variable_name
-        self.variable_value = stick_variable_value
+        # self.variable_value = stick_variable_value
+    
+
+    def generate_card(self, card_number):
+
+        card_dict = {}
+        card_dict["card_type"] = self.card_id
+        card_dict["card_number"] = card_number
+
+        set_text = {
+            "val_type":"text",
+            "text":"Set"
+        }
+        sticker_text = {
+            "val_type": "sticker",
+            "sticker_name": "variable",
+            "sticker_value": self.variable_name
+        }
+        to_text = {
+            "val_type": "text",
+            "text": "to"
+        }
+
+        card_dict["values"] = [set_text, sticker_text, to_text]
+
+        return card_dict
+
 
 if __name__ == "__main__":
-    test_card = VariableSetter("count",10)
-    print(test_card.card_id)
+    test_card = VariableSetter("count")
+    print(test_card.generate_card(1))
